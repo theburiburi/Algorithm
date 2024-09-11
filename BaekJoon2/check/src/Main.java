35c3011da0283da2ccee0264e1b22b7d658e5459
jouijae <kuda1390@dgu.ac.kr>
2024-09-11 23:53:52 +0900

    study

--- BaekJoon2/check/src/Main.java
+++ BaekJoon2/check/src/Main.java
@@ -1,60 +1,89 @@
-import java.io.IOException;
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+
+import java.util.StringTokenizer;
 
 public class Main{
-    static ArrayList<Integer> graph[];
-    static boolean[] visited;
-    static int root;
-    static int delete;
-    static int ans;
+    static int N;
+    static int answer = 0;
+    static int time = 0;
+    static int playerScore[][];
 
-    public static void main(String[] args) throws IOException{
+    static boolean checkPlayer[];
+    static int orderNum[];
+    static boolean currentState[];
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        graph = new ArrayList[N];
-        visited = new boolean[N];
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
 
+        playerScore = new int[N][9];
+        checkPlayer = new boolean[9];
+        orderNum = new int[9];
         for (int i=0; i<N; i++){
-            graph[i] = new ArrayList<>();
-        }
-
-        String arr[] = br.readLine().split(" ");
-        delete = Integer.parseInt(br.readLine());
-
-        for (int i=0; i<N; i++){
-            int temp = Integer.parseInt(arr[i]);
-            if(temp == -1){
-                root = i;
-            }
-            else{
-                graph[i].add(temp);
-                graph[temp].add(i);
+            st = new StringTokenizer(br.readLine(), " "); // 새로배운 표현
+            for (int j=0; j<9; j++){
+                playerScore[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        checkPlayer[3] = true;
+        orderNum[3] = 0;
 
-        if(delete == root){
-            System.out.println(0);
-        }
-        else{
-            dfs(root);
-            System.out.println(ans);
-        }
-        
+        makeNum(1);
+        System.out.println(answer);
+
     }
-
-    static void dfs(int current){
-        visited[current] = true;
-        int node=0;
-        for(int temp : graph[current]){
-            if(!visited[temp] && delete != temp){
-                node++;
-                dfs(temp);
+    static void makeNum(int num){
+        if(num == 9){
+            int score = getScore();
+            answer = Math.max(answer, score);
+            return;
+        }
+        for(int i=0; i<9; i++){
+            if(!checkPlayer[i]){
+                checkPlayer[i] = true;
+                orderNum[i] = num;
+                makeNum(num+1);
+                checkPlayer[i] = false;
             }
         }
-        if (node == 0) ans++;
+    }
+    static int getScore(){
+        int ans=0;
+        time = 0;
+        for(int i=0; i<N; i++){
+            currentState = new boolean[4];
+            int out = 0; //3아웃 체크용
+            while(out < 3){
+                int batter = orderNum[time];
+                if(playerScore[i][batter] == 0){
+                    out++;
+                }
+                else{
+                    ans += moveRunner(playerScore[i][batter]);
+                }
+                if(++time >= 9) {time = 0;} //0번째 타자부터 다시
+            }
+        }
+        return ans;
+    }
+    static int moveRunner(int hitScore){
+        int runs = 0;
+        currentState[0] = true;
+
+        for(int i=3; i>=0; i--){
+            if(currentState[i]){
+                int base = i+hitScore;
+                if(base>=4){
+                    runs++;                    
+                }
+                else{
+                    currentState[base] = true;
+                }        
+                currentState[i] = false;
+            }
+        }
+        return runs;
     }
 }
