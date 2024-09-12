8a266f5f94446077c64c02bbd9be5089e813d15e
jouijae <kuda1390@dgu.ac.kr>
2024-09-12 16:47:23 +0900

    bruteforce

--- BaekJoon2/14500.java
+++ BaekJoon2/14500.java
@@ -3,24 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
-public class Main{
+public class 14500{//14500 브루트포스 구현
     static int answer = 0;
     static int N;
     static int M;
     static boolean visited[][];
     static int arr[][];
 
-
     static int dx[] = {1,-1,0,0};
     static int dy[] = {0,0,1,-1};
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        Queue<Integer> q = new LinkedList<>();
+        
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
@@ -36,13 +31,14 @@ public class Main{
         }
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
-                solution(0, 0, i, j);
+                solution(1, arr[i][j], i, j);
+                solution2(1, arr[i][j], i, j);
             }
         }
         System.out.println(answer);
-
     }
-    static void solution(int depth, int sum, int y, int x){
+    static void solution(int depth, int sum, int y, int x){ // 나머지 모양
+        visited[y][x] = true;
         if(depth == 4){
             answer = Math.max(answer, sum);
             return;
@@ -52,9 +48,27 @@ public class Main{
             int j = x+dx[z];
             if (0<= i && i < N && 0<= j && j < M){
                 if(visited[i][j] == false){
-                    q.add()
                     visited[i][j] = true;
                     solution(depth+1, sum + arr[i][j], i, j);
+                    solution2(depth+1, sum + arr[i][j], i, j);
+                    visited[i][j] = false;
+                }
+            }
+        }
+    }
+    static void solution2(int depth, int sum, int y, int x){ // ㅗ ㅓ ㅏ ㅜ 모양 만들기,,
+        visited[y][x] = true;
+        if(depth == 4){
+            answer = Math.max(answer, sum);
+            return;
+        }
+        for(int z=0; z<4; z++){
+            int i = y+dy[z];
+            int j = x+dx[z];
+            if (0<= i && i < N && 0<= j && j < M){
+                if(visited[i][j] == false){
+                    visited[i][j] = true;
+                    solution(depth+1, sum + arr[i][j], y, x);
                     visited[i][j] = false;
                 }
             }
