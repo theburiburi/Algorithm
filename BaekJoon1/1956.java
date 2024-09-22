3b43d207c04b8cf6681360e1a9e97f6124e5e611
theburiburi <kuda1390@dgu.ac.kr>
2024-09-22 16:58:40 +0900

    tree || bruteforce

---
+++ BaekJoon1/1956.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 1956{//1956 그래프 와샬 
+    static int V, E;
+    static int arr[][];
+    static final int INF = 200000000;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        arr = new int[V+1][V+1];
+        for(int i=1; i<=V; i++){
+            Arrays.fill(arr[i], INF);
+        }
+
+        for(int i=1; i<=E; i++){
+            st = new StringTokenizer(br.readLine());
+            int startNum = Integer.parseInt(st.nextToken());
+            int endNum = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            
+            arr[startNum][endNum] = cost;
+        }
+        System.out.println(wasshal());
+        
+    }
+    static int wasshal(){
+        for(int k=1; k<=V; k++){
+            for (int i=1; i<=V; i++){
+                for(int j=1; j<=V; j++){
+                    if (arr[i][j] > arr[i][k] + arr[k][j]){
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+
+        int ans = INF;
+        for (int i = 1; i <= V; i++) {
+            ans = Math.min(ans, arr[i][i]);
+        }
+        return ans == INF ? -1 : ans;
+    }
+}
--- BaekJoon2/check/src/Main.java
+++ BaekJoon2/check/src/Main.java
@@ -1,76 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main{
-    static int answer = 0;
-    static int N;
-    static int M;
-    static boolean visited[][];
-    static int arr[][];
+public class Main{ //4803 트리 dfs
+    static int num = 1;
+    static int treeNum, N, M;
+    static ArrayList<Integer> graph[];
+    static boolean visited[];
+    static boolean check;
 
-    static int dx[] = {1,-1,0,0};
-    static int dy[] = {0,0,1,-1};
-    public static void main(String[] args)throws IOException{
+    public static void main(String args[])throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[N][M];
-        arr = new int[N][M];
-        
-        for(int i=0; i<N; i++){
+        StringBuilder sb = new StringBuilder();
+        while (true){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            
+            if (N==0 && M==0) break;
+
+            graph = new ArrayList[N+1];
+            for(int i=0; i<=N; i++){
+                graph[i] = new ArrayList<>();
+            }
+
+            for(int i=0; i<M; i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                graph[a].add(b);
+                graph[b].add(a);
+            }
+            countTree();
+            
+            sb.append("Case ").append(num++).append(": ");
+            switch(treeNum){
+                case(0):
+                    sb.append("No trees.").append("\n");
+                    break;
+                case(1):
+                    sb.append("There is one tree.").append("\n");
+                    break;
+                default://else
+                    sb.append("A forest of ").append(treeNum).append(" trees.").append("\n");
+                    break;
             }
         }
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                solution(1, arr[i][j], i, j);
-                solution2(1, arr[i][j], i, j);
-            }
-        }
-        System.out.println(answer);
+        System.out.println(sb.toString());
     }
-    static void solution(int depth, int sum, int y, int x){
-        visited[y][x] = true;
-        if(depth == 4){
-            answer = Math.max(answer, sum);
-            return;
-        }
-        for(int z=0; z<4; z++){
-            int i = y+dy[z];
-            int j = x+dx[z];
-            if (0<= i && i < N && 0<= j && j < M){
-                if(visited[i][j] == false){
-                    visited[i][j] = true;
-                    solution(depth+1, sum + arr[i][j], i, j);
-                    solution2(depth+1, sum + arr[i][j], i, j);
-                    visited[i][j] = false;
+    static void countTree(){
+        treeNum = 0;
+        visited = new boolean[N+1];
+        for(int i=1; i<=N; i++){
+            if(!visited[i]){
+                treeNum++;
+
+                check = true;
+                dfs(i, 0);
+                if (!check){
+                    treeNum--;
                 }
             }
         }
     }
-    static void solution2(int depth, int sum, int y, int x){
-        visited[y][x] = true;
-        if(depth == 4){
-            answer = Math.max(answer, sum);
+    static void dfs(int i, int previous){
+        if(visited[i] == true){
             return;
         }
-        for(int z=0; z<4; z++){
-            int i = y+dy[z];
-            int j = x+dx[z];
-            if (0<= i && i < N && 0<= j && j < M){
-                if(visited[i][j] == false){
-                    visited[i][j] = true;
-                    solution(depth+1, sum + arr[i][j], y, x);
-                    visited[i][j] = false;
-                }
+        visited[i] = true;
+        for(int temp: graph[i]){
+            if(!visited[temp]){
+                dfs(temp, i);
+            }
+            else{
+                if(previous != temp) check = false; // 사이클이 생기는 경우
             }
         }
     }
