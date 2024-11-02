de41f55f6976118e8cca5c56cc4a68c0b280ced2
jouijae <kuda1390@dgu.ac.kr>
2024-11-02 15:09:24 +0900

    String, KMP

--- BaekJoon2/check/src/Main.java
+++ BaekJoon2/check/src/Main.java
@@ -1,70 +1,37 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringTokenizer;
 
-public class Main {
-    static int N;
-    static int DDD, hh, mm;
-    static int F;
-
-    public static void main(String[] args) throws Exception {
+public class Main{//String , KMP
+    public static void main(String args[])throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        String line[] = br.readLine().split(" ");
-        HashMap <String, String> rent = new HashMap<>();
-        HashMap <String, Long> fine = new HashMap<>();
-        
-        N = Integer.parseInt(line[0]);
-        DDD = Integer.parseInt(line[1].substring(0, 3));
-        hh = Integer.parseInt(line[1].substring(4,6));
-        mm = Integer.parseInt(line[1].substring(7,9));
-        F = Integer.parseInt(line[2]);
 
-        long rentRange = DDD*1440 + hh*60 + mm;
+        String sentence = br.readLine();
+        int maxLen = 0;
 
-        for(int i = 0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            String date = st.nextToken();
-            String time = st.nextToken();
-            String stuff = st.nextToken();
-            String user = st.nextToken();
-            String id = user + "_" + stuff;
+        int senLen = sentence.length();
+        for(int i=0; i<senLen; i++){
+            String subString = sentence.substring(i, senLen);
+            maxLen = Math.max(maxLen, kmp(subString));
+        }
+        System.out.println(maxLen);
+    }
+    static int kmp(String subString){
+        int max = 0;
+        int LI = 0;
+        int subLen = subString.length();
+        int pi[] = new int[subLen];
 
-            if(rent.containsKey(id)){
-                String stringTime1 = rent.get(id);
-                String stringTime2 = date+" "+time;
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-
-                long rentTime = (sdf.parse(stringTime2).getTime() - sdf.parse(stringTime1).getTime()) / (1000*60);
-                if(rentRange < rentTime){
-                    if(fine.containsKey(user)){
-                        fine.put(user, fine.get(user)+(rentTime-rentRange)*F);
-                    }
-                    else{
-                        fine.put(user, (rentTime-rentRange)*F);
-                    }
-                }
-                rent.remove(id);
+        for(int RI=1; RI<subLen; RI++){
+            while(LI>0 && subString.charAt(LI) != subString.charAt(RI)){
+                LI = pi[LI-1];
             }
-            else{
-                rent.put(id, date+" "+time);
+            if(subString.charAt(LI) == subString.charAt(RI)){
+                pi[RI] = ++LI;
+                max = Integer.max(max, pi[RI]);
             }
         }
 
-        if(fine.size() > 0){
-            ArrayList <String> al = new ArrayList<>(fine.keySet());
-            StringBuilder sb = new StringBuilder();
-            al.sort((s1,s2)->s1.compareTo(s2));
-            for(String temp : al){
-                sb.append(temp+" "+fine.get(temp)).append("\n");
-            }
-            System.out.println(sb.toString());
-        }
-        else{
-            System.out.println(-1);
-        }
+        return max;
     }
 }
