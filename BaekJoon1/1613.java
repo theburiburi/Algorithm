277e4a2d398a3666fc4e1a052fc4827b64ca7fe6
jouijae <kuda1390@dgu.ac.kr>
2024-11-10 16:29:52 +0900

    wasshal

--- BaekJoon1/1613.java
+++ BaekJoon1/1613.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-public class Main {
+public class 1613 {//1613 와샬 그래프
     static int N, K, S;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
