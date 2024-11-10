416bf314c1415a46dc7a1498615c887a21dbe963
theburiburi <kuda1390@dgu.ac.kr>
2024-11-10 15:52:41 +0900

    deque

--- BaekJoon2/check/src/Main.java
+++ BaekJoon2/check/src/Main.java
@@ -1,26 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String args[])throws IOException{
+public class Main {
+    static int N;
+    static Deque<Integer> deque;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        while(true){
-            String sen = br.readLine();
-            if(sen.equals("0")){
-                break;
-            }
-            StringBuilder temp = new StringBuilder(sen);
-            temp.reverse();
-            String sen2 = temp.toString();
-            if(sen.equals(sen2)){
-                sb.append("yes").append("\n");
+
+        N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        list.sort((s1,s2)->s2 - s1);
+        deque = new ArrayDeque<>();
+
+        for(int i=0; i<N; i++){
+            if(i%2 == 0){
+                deque.addFirst(list.get(i));
             }
             else{
-                sb.append("no").append("\n");
+                deque.addLast(list.get(i));
             }
         }
-        System.out.println(sb.toString());
+
+        int total=0;
+        Integer dequeArr[] = deque.stream().toArray(Integer[]::new);
+        for (int i=0; i<N; i++){
+            int temp = deque.pollFirst();
+            sb.append(temp).append(" ");
+            total += dequeArr[i]*dequeArr[(i+1)%N];
+        }
+        System.out.println(total);
+        System.out.println(sb.toString());  
     }
 }
