e9a9abf33f7aa6e62adf1dc4ec4e2dfc48827d49
theburiburi <kuda1390@dgu.ac.kr>
2024-12-30 11:39:01 +0900

    dp

--- BaekJoon1/2565.java
+++ BaekJoon1/2565.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {//2565 dp
+public class 2565 {//2565 dp
         public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
