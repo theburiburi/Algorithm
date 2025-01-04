03f4076bcb32d96182223bb5ad1c6a5cf2dacb1f
jouijae <kuda1390@dgu.ac.kr>
2025-01-04 16:27:09 +0900

    greedy

--- BaekJoon2/check/src/Main.java
+++ BaekJoon2/check/src/Main.java
@@ -1,29 +1,73 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {//1339 그리디
+public class Main {//1774 그리디
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        List<Integer> negList = new ArrayList<>();
+        List<Integer> posList = new ArrayList<>();
         
-        Integer weight[] = new Integer[26];
-        Arrays.fill(weight, 0);
         for(int i=0; i<N; i++){
-            String sen = br.readLine();
-            int len = sen.length();
-            
-            for(int j=0; j<len; j++){
-                weight[sen.charAt(j)-'A'] += (int)Math.pow(10, len-1-j);
+            int temp = Integer.parseInt(br.readLine());
+            if(temp > 0){
+                posList.add(temp);
+            }
+            else{
+                negList.add(temp);
             }
         }
-        Arrays.sort(weight, Comparator.reverseOrder());
-        int num = 9;
-        int sum = 0;
-        for(int i=0; i<26; i++){
-            if(weight[i] == 0) break;
-            sum += weight[i]*num;
-            num--;
+        posList.sort((s1,s2) -> s2-s1);
+        negList.sort((s1,s2) -> s1-s2);
+
+        int posLen = posList.size();
+        int negLen = negList.size();
+
+        int sum=0;
+        if(posLen == 1 && negLen == 1){
+            if(posList.get(0)*negList.get(0)> posList.get(0)+negList.get(0)){
+                sum += posList.get(0)*negList.get(0);
+            }
+            else sum += posList.get(0)+negList.get(0);
         }
+        else if (posLen == 1){
+            sum += posList.get(0);
+        }
+        else if (negLen == 1){
+            sum += negList.get(0);
+        }
+
+        for(int j=0; j<posLen-1; j++){
+            if(posList.get(j) * posList.get(j+1) > posList.get(j) + posList.get(j+1)){
+                sum += posList.get(j) * posList.get(j+1);
+                j++;
+                if(j==posLen-2){
+                    sum += posList.get(j+1);
+                }
+            }
+            else{
+                sum += posList.get(j);
+                if(j==posLen-2){
+                    sum += posList.get(j+1);
+                }
+            }
+        }
+        for(int j=0; j<negLen-1; j++){
+            if(negList.get(j) * negList.get(j+1) > negList.get(j) + negList.get(j+1)){
+                sum += negList.get(j) * negList.get(j+1);
+                j++;
+                if(j==posLen-2){
+                    sum += posList.get(j+1);
+                }
+            }
+            else{
+                sum += negList.get(j);
+                if(j==posLen-2){
+                    sum += negList.get(j+1);
+                }
+            }
+        }
+            
         System.out.println(sum);
     }
 }
