3b30dfc15236efc0aeafdf805f443231440a33d2
jouijae <kuda1390@dgu.ac.kr>
2025-04-07 18:39:15 +0900

    hashset

--- BaekJoon1/check/src/Main.java
+++ BaekJoon1/check/src/Main.java
@@ -1,52 +1,83 @@
 import java.io.*;
 import java.util.*;
 
-public class Main { //19700 해시
-    static Map<Integer, Integer> map = new HashMap<>();
-    static Node list[];
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        list = new Node[N];
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            int h = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            list[i] = new Node(h,k);
-        }
-        Arrays.sort(list);
-
-        TreeSet<Integer> ts = new TreeSet<>();
-        int group[] = new int[N+1];
-        group[0] = N;
-        for(Node now : list){
-            Integer find = ts.lower(now.k);
-            find = find == null ? 0 : find;
-            if(--group[find] == 0){
-                ts.remove(find);
-            }
-            group[find+1]++;
-            ts.add(find+1);
+public class Main {
+    static class Point {
+        int x, y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
 
-        int ans = 0;
-        for(int i=1; i<=N; i++){
-            ans += group[i];
-        }
-        System.out.println(ans);
-
-    }
-    static class Node implements Comparable<Node>{
-        int h;
-        int k;
-        public Node(int h, int k){
-            this.h = h;
-            this.k = k;
-        }
         @Override
-        public int compareTo(Node o){
-            return o.h - h;
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Point)) return false;
+            Point p = (Point) o;
+            return x == p.x && y == p.y;
         }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            Point[] points = new Point[N];
+            HashSet<Point> pointsSet = new HashSet<>();
+            int ans = 0;
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                points[i] = new Point(x, y);
+                pointsSet.add(points[i]);
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    Point p1 = points[i];
+                    Point p2 = points[j];
+
+                    int dx = p2.x - p1.x;
+                    int dy = p2.y - p1.y;
+                    int len = dx * dx + dy * dy;
+
+                    // 90도 회전한 정사각형 확인
+                    int x3 = p1.x - dy;
+                    int y3 = p1.y + dx;
+                    int x4 = p2.x - dy;
+                    int y4 = p2.y + dx;
+
+                    if (pointsSet.contains(new Point(x3, y3)) && pointsSet.contains(new Point(x4, y4))) {
+                        ans = Math.max(ans, len);
+                    }
+
+                    // 반대 방향 90도 회전도 확인
+                    int x5 = p1.x + dy;
+                    int y5 = p1.y - dx;
+                    int x6 = p2.x + dy;
+                    int y6 = p2.y - dx;
+
+                    if (pointsSet.contains(new Point(x5, y5)) && pointsSet.contains(new Point(x6, y6))) {
+                        ans = Math.max(ans, len);
+                    }
+                }
+            }
+
+            sb.append(ans).append("\n");
+        }
+
+        System.out.print(sb);
     }
 }
