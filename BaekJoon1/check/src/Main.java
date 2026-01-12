9704f4e1fb4e143710975107c36675ab470d8c61
uijae cho <kuda1390@naver.com>
2026-01-12 21:11:57 +0900

    16719 String dfs

--- BaekJoon1/check/src/Main.java
+++ BaekJoon1/check/src/Main.java
@@ -1,59 +1,43 @@
 import java.io.*;
 import java.util.*;
 
-public class Main { //2287 treeset
-    static int K;
-    static int arr[];
-    static HashSet<Integer> dp[] = new HashSet[9];
-    public static void main(String[] args) throws IOException { 
+public class Main { //16719
+    static int N;
+    static String answer = "";
+    static String str = "";
+    static boolean check[];
+    static int count = 0;
+    static StringBuilder sb;
+    public static void main(String args[]) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
 
-        K = Integer.parseInt(br.readLine());
-        int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        for(int i=0; i<n; i++){
-            int num = Integer.parseInt(br.readLine());
-            arr[i] = num;
-        }
-        int num = 0;
+        str = br.readLine();
+        N = str.length();
+        check = new boolean[N];
 
-        for(int i=1; i<=8; i++){
-            dp[i] = new HashSet<>();
-            num = num*10 + K;
-            dp[i].add(num);
-        }
-        solution();
-        
-        for(int i=0; i<n; i++){
-            int ans = 0;
-            for(int j=1; j<=8; j++){
-                if(dp[j].contains(arr[i])){
-                    ans = j;
-                    break;
-                }
-            }
-            sb.append(ans == 0 ? "NO" : ans + "\n");
-        }
-        System.out.print(sb.toString().trim());
+        dfs(0, N-1);
+        System.out.println(sb);
     }
-    public static void solution(){
-        for(int i=1; i<=8; i++){
-            for(int j=1; j<=i/2; j++){
-                int idx = i-j;
-                for(int a : dp[j]){
-                    for(int b : dp[idx]){
-                        dp[i].add(a+b);
-                        dp[i].add(a-b);
-                        dp[i].add(b-a);
-                        dp[i].add(a*b);
-    
-                        if(a!=0){dp[i].add(b/a);}
-                        if(b!=0){dp[i].add(a/b);}
-                    }
-                }
+    public static void dfs(int leftIdx, int rightIdx){
+        if(leftIdx > rightIdx) return;
+
+        int next = leftIdx;
+        for(int j = leftIdx; j<=rightIdx; j++){
+            if(str.charAt(next) > str.charAt(j)){
+                next = j;
             }
         }
+        
+        check[next] = true;
+
+        for(int i=0; i<N; i++){
+            if(check[i]) sb.append(str.charAt(i));
+        }
+        sb.append("\n");
+        
+
+        dfs(next+1, rightIdx);
+        dfs(leftIdx, next-1);
     }
 }
-
