35568a1406bf469be5c3dd731e4cd6b86c0b5d54
uijae cho <kuda1390@dgu.ac.kr>
2026-02-22 23:49:30 +0900

    feat: 1103

--- BaekJoon1/B1103.java
+++ BaekJoon1/B1103.java
@@ -13,7 +13,11 @@ public class B1103{
     static int[] dx = {1, -1, 0, 0};
     public static void main(String args[])throws IOException{
         inputFile();
-        int ans = dfs(0,0);
+        ans = dfs(0,0);
+        if(cycled) System.out.println(-1);
+        else{
+            System.out.println(ans);
+        }
     }
     static void inputFile()throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,29 +38,34 @@ public class B1103{
 
 
     static int dfs(int y, int x){
-        
         if(visited[y][x]){
             cycled = true;
             return -1;
         }
         
-        if(arr[y][x] != 0) return dp[y][x];
+        if(dp[y][x] != 0) return dp[y][x];
+
         visited[y][x] = true;
-        
         int move = arr[y][x] - '0';
+        int maxMove = 1;
+
         for(int i=0;i<4;i++){
             int ny = y + dy[i] * move;
             int nx = x + dx[i] * move;
+
             if(0> ny || ny >= N || 0>nx || nx >=M || arr[ny][nx] == 'H'){
-                ans = Math.max(ans, 1);
                 continue;
             }
 
-            dfs(ny,nx);
-            if(visited[ny][nx]){
-                cycled = true;
-                return -1;
+            int next = dfs(ny, nx);
+            if(cycled) return 0;
+
+            if(next+1 > maxMove){
+                maxMove = next+1;
             }
         }
+
+         visited[y][x] = false;
+         return dp[y][x] = maxMove;
     }
 }
