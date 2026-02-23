0153e43802db034903be109c3c8e465e4c74530d
cho uijae <kuda1390@naver.com>
2026-02-23 16:29:58 +0900

    19591

---
+++ BaekJoon2/B19591.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class B19591{
+    static StringBuilder sb;
+    public static void main(String args[])throws IOException{
+        inputFile();
+    }
+    static void inputFile()throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+    }
+}
