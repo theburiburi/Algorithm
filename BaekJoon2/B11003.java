2610a28dfb70ac760446208ef5036a37cdbf938d
theburiburi <kuda1390@dgu.ac.kr>
2026-03-05 20:46:04 +0900

    260305

--- BaekJoon2/B11003.java
+++ BaekJoon2/B11003.java
@@ -3,27 +3,40 @@ import java.util.*;
 
 public class B11003{
     static StringBuilder sb;
-    static int L, M;
+    static int N, L;
+    static int arr[];
     public static void main(String args[])throws IOException{
         inputFile();
+        System.out.println(sb.toString());
     }
     static void inputFile()throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());;
 
         sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
         L = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        arr = new int[N+1];
+
         Deque<Integer> dq = new ArrayDeque<>();
 
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<L; i++){
-            int now = Integer.parseInt(st.nextToken());
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-            dq.size()
+        for(int i=1; i<=N; i++){
+            while(!dq.isEmpty() && arr[dq.peekLast()] > arr[i]){
+                dq.removeLast();
+            }
+            
+            dq.addLast(i);
+            if(dq.peekFirst() <= i-L ){
+                dq.removeFirst();
+            }
+
+            sb.append(arr[dq.peekFirst()]).append(" ");
         }
         
     }
