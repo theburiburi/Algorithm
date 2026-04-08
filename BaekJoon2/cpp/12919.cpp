53c1a26d9a003c8eda0c9fa8136d99405abf9f75
uijae <kuda1390@dgu.ac.kr>
2026-04-08 10:41:03 +0900

    260408

---
+++ BaekJoon2/cpp/12919.cpp
@@ -0,0 +1,37 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+string S, T;
+int ans = 0;
+void dfs(string now){
+    if(now.size() == S.size()){
+        if(now == S){
+            ans = 1;
+        }
+        return;
+    }
+
+    if(now.back() == 'A'){
+        now.pop_back();
+        dfs(now);
+        now.push_back('A');
+    }
+
+    if(now.front() == 'B'){
+        reverse(now.begin(), now.end());
+        now.pop_back();
+        dfs(now);
+    }
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> S >> T;
+
+    dfs(T);
+
+    cout << ans;
+}
