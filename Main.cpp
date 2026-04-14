920fa3c6a82ce089e89434a961a49caa667822f8
uijae <kuda1390@dgu.ac.kr>
2026-04-14 10:32:03 +0900

    binary search

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,36 @@
+//#include <bits/stdc++.h>
 
-// #include <bits/stdc++.h>
 #include <iostream>
-#include <string>
 #include <algorithm>
 
 using namespace std;
-string S, T;
-int ans = 0;
-void dfs(string now){
-    if(now.size() == S.size()){
-        if(now == S){
-            ans = 1;
-        }
-        return;
-    }
 
-    if(now.back() == 'A'){
-        now.pop_back();
-        dfs(now);
-        now.push_back('A');
-    }
-
-    if(now.front() == 'B'){
-        reverse(now.begin(), now.end());
-        now.pop_back();
-        dfs(now);
-    }
-}
-
-int N, K;
+long long N;
+int K;
 int main(){
     cin.tie(0)->sync_with_stdio(0);
 
-    cin >> S >> T;
+    cin >> N >> K;
+    
+    long long low = 1;
+    long long high = N*N;
+    long long ans = 0;
+    while(low <= high){
+        long long mid = (low + high)/2;
+        long long count = 0;
 
-    dfs(T);
+        for(int i=1; i<=N; i++){
+            count += min(N, mid/i);
+        }
+
+        if (count >= K){
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
 
     cout << ans;
 }
